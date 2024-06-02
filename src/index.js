68d326f5206db8800f72c49f954806ae022d9c97
M.Victoria Montero <mvictoria.monteroo@gmail.com>
2024-06-02 17:44:46 -0400

    guardado de 02-06-2024 en develop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const resetbutton = document.getElementById("reset-button");
+resetbutton.addEventListener('click',function() {textarea.value="";});
+
+
+const textarea = document.querySelector("textarea[name='user-input']");
+//const texto = textarea.value;
+
+textarea.addEventListener('input', updateCounts);
+
+const charCountElem = document.querySelector('.charCount span');
+const wordCountElem = document.querySelector('.wordCount span');
+
+function updateCounts(evento) {
+    console.log(evento)
+    const text = textarea.value;
+    const charCount = text.length;
+    const wordCount = text.trim() ? text.trim().split(/\s+/).length : 0;
+    
+    charCountElem.textContent = charCount;
+    wordCountElem.textContent = wordCount;    
+}
+
+updateCounts();
